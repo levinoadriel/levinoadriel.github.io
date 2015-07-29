1a94eb44d4c6e08a52ddff63ac15d1af773ac1a6
levinoadriel <levino.adriel@gmail.com>
2015-07-29 19:08:44 +0800

    levino

---
+++ js/core.js
@@ -0,0 +1,9 @@
+$(function () {
+  $.backstretch([
+    '/images/bg/1.jpeg',
+    '/images/bg/2.jpeg',
+    '/images/bg/3.jpeg',
+    '/images/bg/4.jpeg',
+    '/images/bg/5.jpeg',
+  ], {duration: 3000, fade: 750});
+});
