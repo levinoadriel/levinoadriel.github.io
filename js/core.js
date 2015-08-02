602edc0b3db1bd1536974869bde28ad2ee918b07
levinoadriel <levino.adriel@gmail.com>
2015-08-02 17:05:04 +0800

    website

--- js/core.js
+++ js/core.js
@@ -1,5 +1,26 @@
 $(function () {
 
-	
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: false
+    },
+    // zoom: {
+    //   enabled: true,
+    //   duration: 300 // don't foget to change the duration also in CSS
+    // }
+  });
+
+  $('.img').hover(function () {
+    $(this).find('.overlay').fadeIn('fast');
+    $(this).find('.image-popup').fadeIn('fast');
+  }, function () {
+    $(this).find('.overlay').fadeOut('fast');
+    $(this).find('.image-popup').fadeOut('fast');
+  });
 
 });
